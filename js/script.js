51462b72750fb8d3eb9a28b834be21e31be4a569
lukaszplac123 <lukasz.plac@gmail.com>
2017-07-28 19:05:34 +0200

    First release. Project 8.5

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var a = prompt('Enter triangle bottom edge length: ');
+var h = prompt('Enter triangle height: ');
+var answerText = "Triangle area is : ";
+var triangleArea = a*h/2;
+console.log(answerText + triangleArea);
+alert(answerText + triangleArea);
